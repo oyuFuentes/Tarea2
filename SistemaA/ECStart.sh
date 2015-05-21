6bdac42fec9ce2cbaf8a738e077bac4062aa75ee
Ramon Badillo <ramonbadillo@Ramons-MacBook-Pro.local>
2015-05-21 12:59:07 -0500

    sh cambiados

--- SistemaA/ECStart.sh
+++ SistemaA/ECStart.sh
@@ -4,20 +4,20 @@ echo "Starting ECS System"
 
 echo "ECS Monitoring Console"
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java ECSConsole"'
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java ECSConsole '$1'"'
 %ECHO Starting Temperature Controller Console
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java TemperatureController"'
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java TemperatureController '$1'"'
 echo "Starting Humidity Sensor Console"
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java HumidityController"'
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java TemperatureSensor"'
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java HumidityController '$1'"'
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java TemperatureSensor '$1'"'
 echo "Starting Humidity Sensor Console"
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java HumiditySensor"'
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java HumiditySensor '$1'"'
 
 echo "ECS Monitoring Security Console"
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java SecurityConsole"'
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java SecurityConsole '$1'"'
 
 echo "Starting Security Sensor Console"
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java SecuritySensor"'
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java SecuritySensor '$1'"'
 
 echo "Starting Security Controller Console"
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java SecurityController"'
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java SecurityController '$1'"'
--- SistemaC/EAllStart.sh
+++ SistemaC/EAllStart.sh
@@ -4,36 +4,36 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo "Starting ECS System"
 
 echo "ECS Monitoring Console"
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java ECSConsole"' 
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java ECSConsole '$1'"'
 echo "Starting Temperature Controller Console"
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java TemperatureController"' 
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java TemperatureController '$1'"'
 echo "Starting Humidity Sensor Console"
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java HumidityController"' 
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java TemperatureSensor"' 
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java HumidityController '$1'"'
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java TemperatureSensor '$1'"'
 echo "Starting Humidity Sensor Console"
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java HumiditySensor"' 
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java HumiditySensor '$1'"'
 echo "ECS Monitoring Security Console"
 
 echo "==SECURITY SYSTEM ==========================================="
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java SecurityConsole"' 
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java SecurityConsole '$1'"'
 echo "Starting Security Controller"
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java SecurityController"' 
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java SecurityController '$1'"'
 echo "Starting Security DoorSensor"
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java DoorSensor"' 
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java DoorSensor '$1'"'
 echo "Starting Security WindowsSensor"
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java WindowSensor"' 
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java WindowSensor '$1'"'
 echo "Starting Security Sensor Console"
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java MovementSensor"' 
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java MovementSensor '$1'"'
 
 echo "==FIRE DETECTION ============================================"
 echo "Starting Fire Console"
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java FireConsole"' 
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java FireConsole '$1'"'
 echo "Starting Alarm Controller Console"
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java AlarmController"' 
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java AlarmController '$1'"'
 echo "Starting Spray Controller Console"
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java SprayController"' 
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java SprayController '$1'"'
 echo "Starting Fire Sensor"
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java FireSensor"' 
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java FireSensor '$1'"'
 echo "==MONITORING DEVICES========================================="
 echo "Starting Service Console"
-osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java ServiceConsole"' 
+osascript -e 'tell app "Terminal" to do script "cd '${DIR}' && java ServiceConsole '$1'"'
